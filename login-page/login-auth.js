9c156286b899af7fc772f0af711d5ffa3948d816
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-03-15 21:48:41 +0500

    coding

--- login-page/login-auth.js
+++ login-page/login-auth.js
@@ -3,7 +3,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/fireba
 
 import {
     getAuth,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 
 const firebaseConfig = {
@@ -20,16 +21,73 @@ const auth = getAuth(app);
 
 //-------------------------------------------------------==> Signed up <== ------------------------------------------------//
 
+
+const signUpbutton = document.getElementById('registerBtn');
+signUpbutton.addEventListener('click', signUp)
+
+
 function signUp() {
-    createUserWithEmailAndPassword(auth, email, password)
+
+    const signupgmail = document.getElementById('signupEmail').value
+    const signuppassword = document.getElementById('signupPassword').value
+
+    // Email format regex
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (signupgmail === "" || signuppassword === "") {
+        alert("Please fill all fields");
+
+    }
+    else if (!emailPattern.test(signupgmail)) {
+
+        alert("Please enter a valid email address");
+
+    }
+    else if (signuppassword.length < 6) {
+
+        alert("Password must be at least 6 characters");
+
+    }
+    else {
+
+        createUserWithEmailAndPassword(auth, signupgmail, signuppassword)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+
+                if (user) {
+                    alert("Account created successfully");
+                }
+                console.log(user)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
+}
+
+//-------------------------------------------------------==> Signed In <== ------------------------------------------------//
+
+
+const loginbutton = document.getElementById('loginBtn');
+loginbutton.addEventListener('click', signIn)
+
+
+function signIn() {
+    const signinemail = document.getElementById('signinEmail').value
+    const signinpassword = document.getElementById('signinPassword').value
+
+
+    signInWithEmailAndPassword(auth, signinemail, signinpassword)
         .then((userCredential) => {
-            // Signed up 
+            // Signed in 
             const user = userCredential.user;
-            // ...
+           console.log(user)
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            // ..
+            console.log(errorMessage)
         });
 }
