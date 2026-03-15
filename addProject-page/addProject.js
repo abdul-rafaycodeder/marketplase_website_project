bdba4af8d768b15294051bf1219c5acf3d0f5d96
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-03-16 01:15:54 +0500

    coding

--- addProject-page/addProject.js
+++ addProject-page/addProject.js
@@ -1,3 +1,65 @@
+// function createCard() {
+
+//     let name = document.getElementById("name").value;
+//     let title = document.getElementById("title").value;
+//     let image = document.getElementById("image").value;
+//     let desc = document.getElementById("desc").value;
+
+//     if (name === "" || title === "" || image === "" || desc === "") {
+//         alert("Please fill all fields");
+//         return;
+//     }
+
+//     let card = `
+// <div class="card">
+
+// <img src="${image}">
+
+// <div class="card-content">
+
+// <h3>${name}</h3>
+
+// <h4>${title}</h4>
+
+// <p>${desc}</p>
+
+// </div>
+
+// </div>
+// `;
+
+//     document.getElementById("cardContainer").innerHTML += card;
+
+// }
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+    getDatabase,
+    ref,
+    push,
+    onValue,
+    remove,
+    update
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAOoM5Mo0h0bkJHdRRT_DCEVIcqMlY7kzo",
+    authDomain: "marketplase-project.firebaseapp.com",
+    databaseURL: "https://marketplase-project-default-rtdb.firebaseio.com",
+    projectId: "marketplase-project",
+    storageBucket: "marketplase-project.firebasestorage.app",
+    messagingSenderId: "539266350649",
+    appId: "1:539266350649:web:d32217bb184a7b6f00d7e8"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const cardRef = ref(db, "cards");
+
+//--------------------------------------------------==> Create Card <==------------------------------------------------//
+
 function createCard() {
 
     let name = document.getElementById("name").value;
@@ -10,25 +72,131 @@ function createCard() {
         return;
     }
 
-    let card = `
-<div class="card">
+    push(cardRef, {
+        name: name,
+        title: title,
+        image: image,
+        desc: desc
+    });
 
-<img src="${image}">
-
-<div class="card-content">
-
-<h3>${name}</h3>
-
-<h4>${title}</h4>
-
-<p>${desc}</p>
-
-</div>
-
-</div>
-`;
-
-    document.getElementById("cardContainer").innerHTML += card;
+    alert("Card Created Successfully");
 
 }
 
+window.createCard = createCard;
+
+
+//--------------------------------------------------==> Realtime Card Load (Refresh ke baad bhi rahega) <==------------------------------------------------//
+
+
+onValue(cardRef, (snapshot) => {
+
+    let container = document.getElementById("cardContainer");
+
+    container.innerHTML = "";
+
+    snapshot.forEach((data) => {
+
+        let card = data.val();
+        let key = data.key;
+
+        container.innerHTML += `
+
+<div class="card">
+
+<img src="${card.image}">
+
+<div class="card-content">
+
+<h3>${card.name}</h3>
+<h4>${card.title}</h4>
+<p>${card.desc}</p>
+
+<button onclick="editCard('${key}')">Edit</button>
+<button onclick="deleteCard('${key}')">Delete</button>
+
+</div>
+
+</div>
+
+`;
+
+    });
+
+});
+
+
+//--------------------------------------------------==> Delete Card (Confirm Popup) <==------------------------------------------------//
+
+window.deleteCard = function (key) {
+
+    let confirmDelete = confirm("Are you sure you want to delete this card?");
+
+    if (confirmDelete) {
+
+        remove(ref(db, "cards/" + key));
+
+        alert("Card Deleted");
+
+    }
+
+}
+
+
+//--------------------------------------------------==> Edit Card Popup <==------------------------------------------------//
+
+let currentKey = "";
+
+window.editCard = function (key) {
+
+    currentKey = key;
+
+    document.getElementById("editPopup").style.display = "flex";
+
+}
+
+window.closePopup = function () {
+
+    document.getElementById("editPopup").style.display = "none";
+
+}
+
+//--------------------------------------------------==> Update Card <==------------------------------------------------//
+
+window.updateCard=function(){
+
+let name=document.getElementById("editName").value;
+let title=document.getElementById("editTitle").value;
+let image=document.getElementById("editImage").value;
+let desc=document.getElementById("editDesc").value;
+
+update(ref(db,"cards/"+currentKey),{
+
+name:name,
+title:title,
+image:image,
+desc:desc
+
+});
+
+alert("Card Updated Successfully");
+
+closePopup();
+
+}
+
+//--------------------------------------------------==> Image / Video Card Toggle <==------------------------------------------------//
+
+window.showImage=function(){
+
+document.getElementById("imageForm").style.display="block";
+document.getElementById("videoForm").style.display="none";
+
+}
+
+window.showVideo=function(){
+
+document.getElementById("imageForm").style.display="none";
+document.getElementById("videoForm").style.display="block";
+
+}
--- addProject-page/library.js
+++ addProject-page/library.js
@@ -1,35 +1,35 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase,ref,onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyAOoM5Mo0h0bkJHdRRT_DCEVIcqMlY7kzo",
-authDomain: "marketplase-project.firebaseapp.com",
-databaseURL: "https://marketplase-project-default-rtdb.firebaseio.com",
-projectId: "marketplase-project",
-storageBucket: "marketplase-project.firebasestorage.app",
-messagingSenderId: "539266350649",
-appId: "1:539266350649:web:d32217bb184a7b6f00d7e8"
+    apiKey: "AIzaSyAOoM5Mo0h0bkJHdRRT_DCEVIcqMlY7kzo",
+    authDomain: "marketplase-project.firebaseapp.com",
+    databaseURL: "https://marketplase-project-default-rtdb.firebaseio.com",
+    projectId: "marketplase-project",
+    storageBucket: "marketplase-project.firebasestorage.app",
+    messagingSenderId: "539266350649",
+    appId: "1:539266350649:web:d32217bb184a7b6f00d7e8"
 
 };
 
-const app=initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const db=getDatabase(app);
+const db = getDatabase(app);
 
-const cardRef=ref(db,"cards");
+const cardRef = ref(db, "cards");
 
-onValue(cardRef,(snapshot)=>{
+onValue(cardRef, (snapshot) => {
 
-let container=document.getElementById("libraryCards");
+    let container = document.getElementById("libraryCards");
 
-container.innerHTML="";
+    container.innerHTML = "";
 
-snapshot.forEach((data)=>{
+    snapshot.forEach((data) => {
 
-let card=data.val();
+        let card = data.val();
 
-container.innerHTML+=`
+        container.innerHTML += `
 
 <div class="card">
 
@@ -42,6 +42,6 @@ container.innerHTML+=`
 
 `;
 
-});
+    });
 
 });
